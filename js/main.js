fd24d92bc0b2196064af3cfe8a475c09b0864315
jonayerdi <jon_ayerdi_33@hotmail.es>
2016-11-29 21:38:31 +0100

    Board font-size adjusted and js restructured

--- js/main.js
+++ js/main.js
@@ -1,20 +1,4 @@
 
-var $minesweeper;
-
-var $lbWidth;
-var $lbHeight;
-var $lbMines;
-
-var currentMode;
-	
-var width;
-var height;
-var mines;
-
-var customWidth = 8;
-var customHeight = 8;
-var customMines = 10;
-
 function applySettings(mode) {
 	switch (mode) {
 		case 'easy':
@@ -70,24 +54,6 @@ function checkSettings() {
 	customHeight = height;
 	customMines = mines;
 	buildBoard();
-}
-
-function buildBoard() {
-	var device_width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
-	var device_height = (window.innerHeight > 0) ? window.innerHeight : screen.height;
-	var size = (device_width > device_height) ? device_height/(parseInt(height)+5) : device_width/(parseInt(width)+1);
-	$minesweeper.empty();
-	$minesweeper.append("<table class=\"board-table\"></table>");
-	var $board_table = $(".board-table");
-	for(var i = 0 ; i < height ; i++) {
-		$board_table.append("<tr id=\"row"+i+"\" height='"+size+"' width='"+size+"'></tr>");
-		var $board_row = $("#row"+i);
-		for(var j = 0 ; j < width ; j++) {
-			$board_row.append("<td onclick=\"discover("+i+","+j+")\" id=\"s"+i+"-"+j+"\" height='"+size
-			+"' width='"+size+"' class='undiscovered'></td>");
-		}
-	}
-	initMines(height,width,mines);
 }
 
 $(document).ready(function() {
--- js/minesweeper.js
+++ js/minesweeper.js
@@ -1,5 +1,22 @@
 
-var mines = [];
+var $minesweeper;
+
+var $lbWidth;
+var $lbHeight;
+var $lbMines;
+
+var currentMode;
+	
+var width;
+var height;
+var mines;
+
+var customWidth = 8;
+var customHeight = 8;
+var customMines = 10;
+
+var mineLocations = [];
+var discovered = 0;
 
 //Returns the color for the discovered square
 function getSquareColor(value) {
@@ -34,8 +51,8 @@ function getSquareColor(value) {
 //Returns * if it's a mine, or the number of mines it has around
 function getSquareValue(x,y) {
 	var surroundMines = 0;
-	for(var i = 0 ; i < mines.length ; i++) {
-		var current = mines[i];
+	for(var i = 0 ; i < mineLocations.length ; i++) {
+		var current = mineLocations[i];
 		if(Math.abs(current.x-x)<2 && Math.abs(current.y-y)<2) {
 			if(current.x == x && current.y == y) return '*';
 			else surroundMines++;
@@ -46,27 +63,49 @@ function getSquareValue(x,y) {
 
 //Creates m random positions within x and y
 function initMines(x,y,m) {
-	mines = [];
+	mineLocations = [];
+	discovered = 0;
 	var placedMines = 0;
 	while(placedMines < m) {
 		var mine = {x:Math.floor((Math.random() * x)), y:Math.floor((Math.random() * y))};
 		if(getSquareValue(mine.x, mine.y) != '*') {
-			mines.push(mine);
+			mineLocations.push(mine);
 			placedMines++;
 		}
 	}
 }
 
+//Builds the board table
+function buildBoard() {
+	var device_width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+	var device_height = (window.innerHeight > 0) ? window.innerHeight : screen.height;
+	var size = (device_width > device_height) ? device_height/(parseInt(height)+3) : device_width/(parseInt(width)+1);
+	$minesweeper.empty();
+	$minesweeper.append("<table class=\"board-table\"></table>");
+	var $board_table = $(".board-table");
+	for(var i = 0 ; i < height ; i++) {
+		$board_table.append("<tr id=\"row"+i+"\" height='"+size+"' width='"+size+"'></tr>");
+		var $board_row = $("#row"+i);
+		for(var j = 0 ; j < width ; j++) {
+			$board_row.append("<td onclick=\"discover("+i+","+j+")\" id=\"s"+i+"-"+j+"\" height='"+size
+			+"' width='"+size+"' class='undiscovered' style=\"font-size:"+size/2+"px;\"></td>");
+		}
+	}
+	initMines(height,width,mines);
+}
+
 //Discovers all mines (you lost)
 function discoverAllMines() {
-	for(var i = 0 ; i < mines.length ; i++) {
-		var m = mines[i];
+	for(var i = 0 ; i < mineLocations.length ; i++) {
+		var m = mineLocations[i];
 		$square = $('#s'+m.x+"-"+m.y);
 		$square.removeClass("undiscovered");
 		$square.html('*');
 		$square.addClass("boom");
 	}
-	$(".undiscovered").removeClass("undiscovered");
+	var $undiscovered = $(".undiscovered");
+	$undiscovered.addClass("disabled");
+	$undiscovered.removeClass("undiscovered");
 }
 
 //Discovers the square
@@ -92,6 +131,7 @@ function discover(x,y) {
 		}
 		else {
 			$square.css("color",getSquareColor(squareValue));
+			discovered++;
 		}
 	}
 }
